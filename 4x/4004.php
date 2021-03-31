3feccb526b52c4fc1475982b2571f7c99b1e3af1
Piotr Badełek <piotrbadelek@protonmail.com>
2021-03-31 14:41:35 +0200

    cement yes

---
+++ 4x/4004.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	"name" => "4004 Ben is being naughty again",
+	"signal" => "error",
+	"description" => "Ben is being naughty again (eating cement?).",
+	"causes" => ["Ben Awad is eating cement"]
+];
